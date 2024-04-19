85fad965028b07204a79ed70ed54c8fa84f920f5
giorgia barachetti <giorgiabarachetti@gmail.com>
2024-04-19 10:20:01 +0200

    start project

---
+++ src/components/TooltipItem.js
@@ -0,0 +1,27 @@
+import {AppBar, Button, IconButton, Toolbar, Typography} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Sidebar = () => {
+    return (<>
+        <AppBar positionn='static'>
+            <Toolbar>
+
+                <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                    titolo TODO ADD PROPS
+                </Typography>
+                <Button color="inherit">Login</Button>
+                <Button color="inherit">Login</Button>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ml: 2}}
+                >
+                    <MenuIcon/>
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    </>)
+}
+export default Sidebar;
---
+++ src/pages/patient/Doctor.js
@@ -0,0 +1,77 @@
+import IdentityCard from "../../components/IdentityCard";
+import GeneralTable from "../../components/GeneralTable";
+import {Box, Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
+const Doctor = () => {
+    const navigate = useNavigate()
+
+    const columns = [
+        {id: 'date', label: 'Data visita', align: 'left'},
+        {id: 'medico', label: 'Medico', align: 'left'},
+        {id: 'tipologia', label: 'Tipologia', align: 'left'},
+        {id: 'dettagli', label: 'Dettagli', align: 'left'}
+    ]
+
+    const rows = [
+        {
+            date: '2024-04-01',
+            medico: 'Dr. John Doe',
+            tipologia: 'Controllo generale',
+            dettagli: 'Nessun problema riscontrato.'
+        },
+        {
+            date: '2024-03-25',
+            medico: 'Dr. Sarah Smith',
+            tipologia: 'Esame del sangue',
+            dettagli: 'Esito positivo, nessun problema rilevato.'
+        },
+        {
+            date: '2024-03-18',
+            medico: 'Dr. Michael Brown',
+            tipologia: 'Visita oculistica',
+            dettagli: 'Prescrizione per occhiali da vista.'
+        },
+    ]
+
+    const doctorData = {
+        picture: 'https://randomuser.me/api/portraits/men/1.jpg',
+        name: 'John',
+        surname: 'Doe',
+        sex: 'Male',
+        birthDate: '1978-05-15',
+        birthPlace: 'New York',
+        height: '180 cm',
+        taxIdCode: 'ABC123456789',
+        nationality: 'American',
+        address: '123 Main Street, New York, NY',
+        telephoneNumber: '+1 123 456 7890',
+    }
+    /*
+const patientData = {
+    picture: 'https://randomuser.me/api/portraits/women/2.jpg',
+    name: 'Jane',
+    surname: 'Doe',
+    sex: 'Female',
+    birthDate: '1985-08-20',
+    birthPlace: 'Los Angeles',
+    height: '165 cm',
+    taxIdCode: 'XYZ987654321',
+    nationality: 'American',
+    address: '456 Elm Street, Los Angeles, CA',
+    telephoneNumber: '+1 987 654 3210',
+};
+     */
+
+    return (<>
+        <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+            <IdentityCard fields={doctorData}/>
+            <GeneralTable columns={columns} rows={rows}/>
+        </Box>
+        {/*<Button onClick={() => getItem()}>get</Button>*/}
+        <Button onClick={() => navigate('/book-appointment')}>click</Button>
+        <Button onClick={() => navigate('/medical-visit-history')}>click</Button>
+    </>)
+}
+
+export default Doctor;
