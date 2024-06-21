681b768b6f479bca25180b824a593ac609949c8f
AndreaCastelli <mrandrea2008@gmail.com>
2024-06-21 11:50:39 +0200

    vskjo

--- src/pages/doctor/Profile.js
+++ src/pages/doctor/Profile.js
@@ -33,6 +33,11 @@ const Profile = () => {
     const getDataDoctor = async (id) => {
         const token = sessionStorage.getItem('token')
         const config = {headers: {Authorization: `Bearer ${token}`}}
+        // await axios.get(`http://localhost:3333/users/${id}`, config)
+        //     .then((res) => {
+        //         console.log('PROFILE - getbookingresult', res)
+        // setRows(normalizeItem(res.data.data))
+        // })
     }
     const deleteStorage = () => {
         try {
